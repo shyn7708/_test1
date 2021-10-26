4d5fba76668023b94f5fff13f103f610dfdacf84
shyn7708 <shyn7708@naver.com>
2021-10-26 19:39:57 +0900

    commit test Real

---
+++ src/day0730/HelloWorld_01.java
@@ -0,0 +1,12 @@
+package day0730;
+
+public class HelloWorld_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+
+		
+	}
+
+}
